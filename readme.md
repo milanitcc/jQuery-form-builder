5aefa2ace61bee3dfc527d52af8e495caac65813
milanitcc <milan@itccdigital.com>
2024-04-19 14:25:17 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,36 @@
+# jQuery formBuilder
+
+Drag and drop form creation for your app.
+
+Here is the url for the [documents](https://formbuilder.online/docs/).
+
+# Introduction
+
+jQuery formBuilder is a 100% client-side plugin that gives users the power to create forms using an intuitive drag and drop interface. FormBuilder supports a number of form fields and some html tags.
+
+formRender is the companion plugin to formBuilder that lets you render the results of the created form. A typical use case would have formBuilder in an admin area of a site or app and formRender on the front-end. For this reason formRender and formBuilder are 2 separate plugins but can be used together to create an edit toggle.
+
+# Features
+
+* Customization - enable only the fields you need, use your own notifications append or prepend content and more.
+* Bootstrap ready but not dependent
+* Translatable
+
+# Browser Support
+
+# List of built-in controls
+
+* autocomplete
+* button
+* checkbox-group
+* date
+* file
+* header
+* hidden
+* number
+* paragraph
+* radio-group
+* select
+* starRating
+* text
+* textarea
---
+++ script.js
@@ -0,0 +1,77 @@
+jQuery(function ($) {
+    var templates = {
+        break: function (fieldData) {
+            return {
+                field: '<hr class=' + fieldData.className + '>'
+            };
+        },
+        signature: function (fieldData) {
+            return {
+                field: `<section class="signature-component">
+                        <h1>Draw Signature</h1>
+                        <h2>with mouse or touch</h2>
+                        <canvas id="signature-pad" width="400" height="200"></canvas>
+                    <div>
+                        <button id="save">Save</button>
+                        <button id="clear">Clear</button>
+                        <button id="showPointsToggle">Show points?</button>
+                    </div>
+                    <p>
+                    <br />
+                    <a href="https://codepen.io/kunukn/pen/bgjzpb/" target="_blank">Throttling without lag example here</a><br />
+                    <br />
+                    <a href="https://github.com/szimek/signature_pad" target="_blank">Signature Pad</a> with custom Simplifying and Throttling
+                    </p>
+                </section>`
+            }
+        }
+    };
+    $(document.getElementById('fb-editor')).formBuilder({
+        dataType: 'xml',
+        disabledAttrs: ['access'],
+        disableFields: [
+            'autocomplete',
+            'button',
+            'hidden',
+            'number',
+            'select'
+        ],
+        fieldRemoveWarn: false,
+        controlPosition: 'left',
+        inputSets: [
+            {
+                label: 'Section Break',
+                name: 'section_break', // optional - one will be generated from the label if name not supplied
+                showHeader: false, // optional - Use the label as the header for this set of inputs
+                fields: [
+                    {
+                        type: 'break',
+                        label: 'Section Break',
+                    },
+                ]
+            },
+            // {
+            //     label: 'Signature',
+            //     name: 'signature', // optional - one will be generated from the label if name not supplied
+            //     showHeader: false, // optional - Use the label as the header for this set of inputs
+            //     fields: [
+            //         {
+            //             type: 'signature',
+            //             label: 'Signature',
+            //         },
+            //     ]
+            // },
+        ],
+        disabledFieldButtons: {
+            break: ['edit'], // disables the remove butotn for text fields
+            signature: ['edit'], // disables the remove butotn for text fields
+        },
+        templates,
+        onSave: function (evt, formData) {
+
+            console.log(formData);
+            // toggleEdit();
+            $('.render-wrap').formRender({ formData });
+        },
+    });
+});
